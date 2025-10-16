23a04f54fe9f1549bfdb7be7891de486d4633af6
marco rojouan <marcoprojouan@outlook.fr>
2025-10-17 01:15:16 +0200

    strtrim cleaned

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,7 +12,31 @@
 
 #include "libft.h"
 
+int	set_startend(const char *s1, const char *set, int *start, int *end)
+{
+	*start = 0;
+	while (s1[*start] && ft_strchr(set, s1[*start]))
+        (*start)++;
+	*end = ft_strlen(s1) - 1;
+	while (*end >= *start && ft_strchr(set, s1[*end]))
+		(*end)--;
+}
+
 char	*ft_strtrim(const char *s1, const char *set)
 {
+	int start;
+    int end
+	int len
+	char *trimstr;
 	
+	if (!s1 || !set)
+        return (NULL);
+    set_startend(s1, set, &start, &end);
+	len = end - start;
+	trimstr = malloc(sizeof(char) * (len + 1))
+	if (!trimstr)
+		return (NULL);
+	trimstr = ft_memcpy(trimstr, s1 + start, len);
+	trimstr[len] = '\0';
+	return (trimstr);
 }
