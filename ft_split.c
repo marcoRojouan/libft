fb66cf439fc9a3bfb9139e559cd284f9f8f19cf3
marco rojouan <marcoprojouan@outlook.fr>
2025-10-19 15:07:02 +0200

    fuck this split

--- ft_split.c
+++ ft_split.c
@@ -12,91 +12,89 @@
 
 #include "libft.h"
 
+void	free_split(char **tab, int j)
+{
+	int	i;
+
+	i = 0;
+	while (i < j)
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+}
+
+int	ft_wordlen(const char *str, char sep)
+{
+	int	len;
+
+	len = 0;
+	while (str[len] && str[len] != sep)
+		len++;
+	return (len);
+}
+
+char	*ft_strndup(const char *str, char sep)
+{
+	int		len;
+	int		i;
+	char	*tab;
+
+	len = ft_wordlen(str, sep);
+	tab = malloc(sizeof(char) * (len + 1));
+	if (!tab)
+		return (NULL);
+	i = 0;
+	while (str[i] && str[i] != sep)
+	{
+		tab[i] = str[i];
+		i++;
+	}
+	tab[i] = '\0';fg
+	return (tab);
+}
+
 int	ft_count_words(const char *str, char sep)
 {
 	int	count;
-	int	in_words;
 	int	i;
 
 	count = 0;
-	in_words = 0;
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] != sep && in_words == 0)
-		{
-			in_words = 1;
+		if ((str[i] != sep) && (i == 0 || str[i - 1] == sep))
 			count++;
-		}
-		else if (str[i] == sep)
-			in_words = 0;
 		i++;
 	}
 	return (count);
 }
 
-char	*ft_strndup(const char *str, int len)
-{
-	int		i;
-	char	*duplicata;
-
-	duplicata = NULL;
-	duplicata = malloc(sizeof(char) * len + 1);
-	if (!duplicata)
-		return (0);
-	i = 0;
-	while (i < len)
-	{
-		duplicata[i] = str[i];
-		i++;
-	}
-	duplicata[i] = '\0';
-	return (duplicata);
-}
-
-char	*just_splitting(const char *str, char sep, int *i)
-{
-	int		start;
-	int		word_len;
-	char	*result;
-
-	result = NULL;
-	while (str[*i] == sep)
-		(*i)++;
-	if (str[*i] != sep)
-	{
-		start = *i;
-		while (str[*i] && str[*i] != sep)
-			(*i)++;
-		word_len = (*i) - start;
-		result = ft_strndup(str + start, word_len);
-	}
-	return (result);
-}
-
-char	**ft_split(char const *str, char sep)
+char	**ft_split(const char *str, char sep)
 {
 	char	**tab;
-	char	*word;
 	int		i;
 	int		j;
-	int		word_count;
 
 	i = 0;
 	j = 0;
-	word_count = ft_count_words(str, sep);
-	tab = NULL;
-	tab = malloc(sizeof(char *) * (word_count + 1));
+	tab = malloc(sizeof(char *) * (ft_count_words(str, sep) + 1));
 	if (!tab)
-		return (0);
+		return (NULL);
 	while (str[i])
 	{
-		word = just_splitting(str, sep, &i);
-		if (word)
+		if ((str[i] != sep) && (i == 0 || str[i - 1] == sep))
 		{
-			tab[j] = word;
+			tab[j] = ft_worddup(str + i, sep);
+			if (!tab[j])
+			{
+				free_split(tab, j);
+				return (NULL);
+			}
 			j++;
 		}
+		i++;
 	}
 	tab[j] = NULL;
 	return (tab);
